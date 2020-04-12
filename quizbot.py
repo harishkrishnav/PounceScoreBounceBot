7c3dafc7966d9465c9def48c49856ffd12a25938
Harish <hakr@microsoft.com>
2020-04-11 17:41:05 -0700

    added aliases, changed number of teams, rewrote banner comment

--- quizbot.py
+++ quizbot.py
@@ -1,49 +1,43 @@
 """
-This is a discord bot to handle pounces and bounces in a quiz. A participant is assigned their team as a role (e.g. 'team1', 'team2') 
-and has access only to the chat and voice channels of that team. They also have access to the general channels, but switching 
-between channels is typically tiresome. This bot tries to make it easier for the participant by giving them no need to move out of 
-their team's text chat channel. 
+For more information, please see the repo README https://github.com/harishkrishnav/PounceScoreBounceBot/blob/master/README.md 
+The guild must have the following with appropriate permissions
+- roles: quizmaster, scorer, team1, team2, ...
+- text channels: bounce-guesses, pounce-guesses, scores
+- text channels: team1, team2, ...
 
-##########################################
-Pouncing
-##########################################
-To pounce, a participant simply types
-!p their pounce answer
-This message is displayed as a popup to the quizmaster and appears in a separate channel for pounce answers that only the
-quizmaster has access to. For example, if a quizzer named harish in team3 wants to submit "mahatma gandhi" as a pounce answer,
-he simply types "!p mahatma gandhi" in team3's channel. The quizmaster would see the message "mahatma gandhi pounced by team3's harish".
+Summary of commands
 
-##########################################
-Bounce
-##########################################
-Similarly, for bounce, a team types in their team's private chat 
-!b their bounce answer
-The bot sends this message to the private chats of all teams. So, if harish of team 3 were to answer "mahatma gandhi" on bounce,
-he would type "!b mahatma gandhi" and all teams would see "Guess on the bounce by team3's harish : mahatma gandhi". This will also appear 
-in a separate bounces channel. 
+- !p or !pounce 
+example: !pounce mahatma gandhi
+pounces-guesses channel sees "mahatma gandhi pounced by <teams>'s <sender's name>"
+sender's channel gets the message "Pounce submitted"
 
-##########################################
-Scoring
-##########################################
-The 'quizmaster' or someone else who is assigned with the role of 'scorer' can simply type "!s 10 t4 t6" to add 10 points to the scores 
-of team4 and team6. Likewise, "!s -15 t1 t8" subtracts 15 points from the scores of team1 and team8. All score updates will be visible on 
-the dedicated scores channel and a message will be sent to every team that has an update. The score channel will also have the full table 
-of points of every team after every update.
+- !b or !bounce
+example: !b mahatma gandhi
+bounces-guesses channel sees "<team>'s <sender's name> : mahatma gandhi"
+every team channel sees "Guess on the bounce by <team>'s <sender's name> : mahatma gandhi"
 
-Typing !scores at any time from any channel will display all the scores. 
+- !scores or !pointstable
+example: !scores
+sender's channel sees "<team>:<score>" one team per line
 
-If there are connectivity issues, the bot may reload and the scores may reset to zero. A warning will be displayed and the scores will have 
-to be entered again. Pounce/bounce should not be affected.  
+- !s or !plus
+example: !s -5 t2 t4 t6
+-5 added to score of team2, team4, team6
+scores-channel sees <points> to <list of teams>. Points table now: <pointstable>
+channels of teams with update see "<points> to your team. Your score is now <total points>"
+action only when sender has role quizmaster, scorer
 
-##########################################
-Cleanup
-##########################################
-This command deletes all messages in all channels with ids defined below
-!clearAllChannels 
-This command deletes all messages in the specific channel it is called
-!clearThis
-Discord has trouble deleting messages older than 14 days. You'd much rather run this command than clone/delete channels and update the 
-channel IDs below.
+- !clearAllChannels
+example: !clearAllChannels
+all messages from all non-whitelisted channels deleted
+all scores reset to 0
+action only when sender has role quizmaster, scorer
+
+- !clearThis
+example !clearThis 
+all messages in that caller's channel are deleted
+action on channels bounce-guesses, pounce-guesses, scores  when sender has role quizmaster, scorer
 
 """
 
@@ -58,15 +52,16 @@ try:
     with open('.env', 'r') as e:
         l = e.readline()
         token = l.split('TOKEN=')[1]
+        assert len(token)>50
         l = e.readline()
         guildId = l.split('GUILD=')[1]
+        assert len(guildId)>16
 
 except FileNotFoundError:
     # otherwise, we take the token and guild ID as inputs, and write to 
     # .env for ease of use next time
     print("Setting up the bot for your server For details, refer to \
-            \nhttps://github.com/harishkrishnav/PounceScoreBounceBot\
-#running-the-bot-the-first-time")
+            \nhttps://github.com/harishkrishnav/PounceScoreBounceBot\#running-the-bot-the-first-time")
 
     token = input("Enter bot developer token: ") 
     # You can find this in https://discordapp.com/developers/applications 
@@ -79,20 +74,14 @@ except FileNotFoundError:
         envfile.write('TOKEN='+token+'\n')
         envfile.write('GUILD='+guildId+'\n')
 except:
-    print("An unknown exception occured. Please contact the developers")
+    print("An unknown exception occured. There might be a problem with the .env file or its contents. Try deleting it and running this script again")
     raise
 
-
-while True:
-    numberOfTeams = int(input("How many teams for the quiz (<= 8)? ")) #teams 
-    # with team numbers greater than this are ignored
-    if numberOfTeams <= 8:
-        break
+numberOfTeams = 8
 bounceChannel = 'bounce-guesses'
 pounceChannel = 'pounce-guesses'
 scoreChannel  = 'scores'
-whitelistChannels = ['general','discord-and-bot-help'] #these channels are not
-# touched by the bot and hence not cleared during reset
+whitelistChannels = ['general','discord-and-bot-help'] #these channels are not touched by the bot and hence not cleared during reset
 #It is assumed that every team has a name like `teamX-chat`
 
 commonChannels = {}
@@ -120,14 +109,13 @@ async def on_ready():
     for team in teamChannels:
         scores[team] = 0
 
-    await broadcastToAllTeams("Welcome to the quiz! \nCommands: `!p <answer>` \
-or `pounce <answer>` to pounce, `!b <answer>` or `!bounce <answer>` to answer \
-on bounce, `!scores` to see scores.")
+    await broadcastToAllTeams("Welcome to the quiz! Commands: \n`!p your guess here` or `!pounce your guess here` to pounce,\n\
+        `!b your guess here` or `!bounce your guess here` to answer on bounce, `!scores` to see the scoretable.")
     
     response = "The bot is ready to bring the pounces to you"
     await commonChannels[pounceChannel].send(response)
     
-    response = "Guesses on bounce you make by with `!b`/`!bounce` command \
+    response = "Guesses on bounce you make by with `!bounce` or `!b` command \
 appear here"
     await commonChannels[bounceChannel].send(response)
     
@@ -135,35 +123,40 @@ appear here"
     await commonChannels[scoreChannel].send(response)
 
 
-@bot.command(name="p", aliases = ['pounce'], help="Pounce: type `!p your guess` or `!pounce your guess` to send \"your guess\" to the quizmaster")
+@bot.command(name="p", aliases = ["pounce", "P", "punce", "ponce", "puonce"], 
+             help="Pounce: type `!p your guess` or `!pounce your guess` to send \"your guess\" to the quizmaster")
 async def pounce(ctx, *args, **kwargs):
-    message = ' '.join([word for word in args])
+    guess = ' '.join([word for word in args])
     author = ctx.message.author
-    team = str([y.name.lower() for y in author.roles][1:])
-    response = '\'{}\' pounced by {}\'s {}'.format(str(message), team, author)
+    team = ', '.join([str(role.name.lower()) for role in author.roles[1:] if role.name.startswith('team')])
+    response = '\'{}\' pounced by {}\'s {}'.format(guess, team, author.split('#')[0])
     channel = commonChannels[pounceChannel]
     await channel.send(response)
+    response = "Pounce submitted"
+    await ctx.message.channel.send(response)
 
-@bot.command(name="b", aliases = ['bounce'], help="Bounce: type `!b your guess` or `!bounce your guess` to send \"your guess\" to the quizmaster and all teams")
+
+@bot.command(name="b", aliases = ["bounce", "B", "bunce", "bonce", "buonce"], 
+             help="Bounce: type `!b your guess` or `!bounce your guess` to send \"your guess\" to the quizmaster and all teams")
 async def bounce(ctx, *args, **kwargs):
-    message = ' '.join([word for word in args])
+    guess = ' '.join([word for word in args])
     author = ctx.message.author
-    team = str([y.name.lower() for y in author.roles][1:])
-    response = '{}\'s {}: {}'.format(team, author, str(message))
+    team = str([role.name.lower() for role in author.roles[1:] if role.name.startswith('team')])
+    response = '{}\'s {}: {}'.format(team, author, guess)
     channel = commonChannels[bounceChannel]
     await channel.send(response)
-    response = 'Guess on the bounce by {}\'s {}: {}'.format(team, author, str(message))
+    response = 'Guess on the bounce by {}\'s {}: {}'.format(team, author, str(guess))
     await broadcastToAllTeams(response)
 
-@bot.command(name="scores", help="Displays the scores")
+@bot.command(name="scores", aliases = ["pointstable"], help="Displays the scores")
 async def displayScores(ctx, *args, **kwargs):
     response = '\n'.join(str(team)+" : "+str(scores[team]) for team in scores)
     await ctx.message.channel.send(response)
 
-@bot.command(name="s", help="scorer or quizmaster updates scores")
+@bot.command(name="s", aliases = ["plus"], help="scorer or quizmaster updates scores")
 async def updateScores(ctx, *args, **kwargs):
     if len(args)<2:
-        await ctx.send("example:   !s -5 t2 t8   to deduct 5 points from team2 and team8")
+        await ctx.send("example: `!s -5 t2 t8`  to deduct 5 points from team2 and team8")
         return
 
     author = ctx.message.author
@@ -187,7 +180,9 @@ async def updateScores(ctx, *args, **kwargs):
         response = '{}{} to your team. Your score is now {}'.format(sign(points),str(points), scores[team])
         await channel.send(response)
 
-@bot.command(name="clearAll", help="delete all messages in all important channel and resets score to 0")
+#TODO something to handle !minus
+
+@bot.command(name="clearAllChannels", help="delete all messages in all important channel and resets score to 0")
 async def clearAll(ctx, *args, **kwargs):
     author = ctx.message.author
     authorRoles = [str(role).lower() for role in author.roles[1:]]
